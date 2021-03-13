3b69e7566d0539d1347e330aef7d259f5c9c2f0c
Jordan Cook <jordan.cook@pioneer.com>
2021-03-13 13:54:50 -0600

    Use log for ranking by obs/id counts (so outliers don't skew results too much)

--- inat_backlog_slogger/__init__.py
+++ inat_backlog_slogger/__init__.py
@@ -1,4 +1,8 @@
 from logging import basicConfig, getLogger
 
-basicConfig(level='INFO')
+basicConfig(
+    level='INFO',
+    format='[%(asctime)s] %(levelname)s - %(name)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+)
 getLogger('pyrate_limiter').setLevel('WARNING')
--- inat_backlog_slogger/ranking.py
+++ inat_backlog_slogger/ranking.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python
 """Utilities for ranking observation data"""
-from inat_backlog_slogger.constants import RANKING_WEIGHTS
+import numpy as np
+
+from inat_backlog_slogger.constants import RANK_BY_NATURAL_LOG, RANKING_WEIGHTS
 from inat_backlog_slogger.observations import load_observations, save_observations
 
 
 def rank_observations(df):
     """Combine normalized and weighted values into a single ranking value, and sort"""
 
-    def normalize(series):
+    def normalize(key, series):
+        series = series.copy()
+        if key in RANK_BY_NATURAL_LOG:
+            with np.errstate(divide='ignore'):
+                series = np.log(series)
+        series[np.isneginf(series)] = 0
         return (series - series.mean()) / series.std()
 
-    df['rank'] = sum([normalize(df[key]) * weight for key, weight in RANKING_WEIGHTS.items()])
+    df['rank'] = sum([normalize(key, df[key]) * weight for key, weight in RANKING_WEIGHTS.items()])
     df['rank'].fillna(0)
     return df.sort_values('rank', ascending=False)
 
