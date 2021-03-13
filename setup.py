bfcdf2960d8349379571cdd2db7f65af71528760
Jordan Cook <jordan.cook@pioneer.com>
2021-03-12 20:41:24 -0600

    Split this into a separate project from pyinaturalist

--- setup.py
+++ setup.py
@@ -1,59 +1,30 @@
 #!/usr/bin/env python
-from itertools import chain
 from setuptools import find_packages, setup
 
-from pyinaturalist import __version__
-
-# These package categories allow tox and build environments to install only what they need
-extras_require = {
-    # Packages used for CI jobs
-    'build': ['coveralls', 'twine', 'wheel'],
-    # Packages used for documentation builds
-    'docs': [
-        'm2r2',
-        'nbsphinx',
-        'prompt_toolkit',
-        'Sphinx~=3.2.1',
-        'sphinx-autodoc-typehints',
-        'sphinx-automodapi',
-        'sphinx-copybutton',
-        'sphinx-rtd-theme',
-        'sphinxcontrib-apidoc',
-    ],
-    # Packages used for testing both locally and in CI jobs
-    'test': [
-        'black==20.8b1',
-        'flake8',
-        'isort',
-        'mypy',
-        'pre-commit',
-        'pytest>=5.0',
-        'pytest-cov',
-        'requests-mock>=1.7',
-        'tox>=3.15',
-    ],
-    # Packages used only for local debugging
-    'debug': ['ipython', 'ipdb', 'prance[osv]', 'rich'],
-}
-# All development/testing packages combined
-extras_require['dev'] = list(chain.from_iterable(extras_require.values()))
+__version__ = '0.0.1'
 
 
 setup(
-    name='pyinaturalist',
+    name='inat-backlog-slogger',
     version=__version__,
-    author='Nicolas Noé',
-    author_email='nicolas@niconoe.eu',
-    url='https://github.com/niconoe/pyinaturalist',
+    author='Jordan Cook',
+    url='https://github.com/JWCook/inat-backlog-slogger',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'keyring~=22.3.0',
-        'pyrate-limiter>=2.3.0; python_version>="3.7"',
-        'python-dateutil>=2.0',
-        'python-forge',
-        'requests>=2.20',
+        'aiofiles',
+        'aiohttp',
+        'pandas',
+        'pyinaturalist==0.13.0.dev221',
+        'rich',
     ],
-    extras_require=extras_require,
-    zip_safe=False,
+    extras_require={
+        'dev': [
+            'black==20.8b1',
+            'flake8',
+            'isort',
+            'mypy',
+            'pre-commit',
+        ],
+    },
 )
